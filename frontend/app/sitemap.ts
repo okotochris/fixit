1e4472d1b304ce7211e4ed9612d497bdf3cf9f0f
okoto.chris <okoto.chis.oc@gmail.com>
2026-06-08 11:05:46 +0100

    site map added

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -3,11 +3,11 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   try {
     const jobs = await fetch(
-      "https://api.servicehub.space/api/jobs/sitemap"
+      "https://servicehub.space/api/jobs/sitemap"
     ).then((res) => res.json());
 
     const professionals = await fetch(
-      "https://api.servicehub.space/api/professionals/sitemap"
+      "https://servicehub.space/api/professionals/sitemap"
     ).then((res) => res.json());
 
     return [
