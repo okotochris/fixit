e0e0c1d2dd5c6e30a7120cbe3034050afa1e8e10
okoto.chris <okoto.chis.oc@gmail.com>
2026-06-08 13:57:55 +0100

    installation updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  ...nextConfig,
-});
+export default nextConfig;
